8ecaf15a25f3d4f673dd01b38152cbb0cc5a6e1d
fahara02 <idea3d.faruk@gmail.com>
2024-06-09 16:25:41 +0600

    modified state machine

--- src/NestMQTT/MQTTCore/MQTTStateMachine.cpp
+++ src/NestMQTT/MQTTCore/MQTTStateMachine.cpp
@@ -1,7 +1,6 @@
 #include "MQTTStateMachine.h"
 #include <ArduinoJson.h>
 #include <LittleFS.h>
-#include <array>
 #include <cstddef>
 #include <cstring>
 #include <functional>
@@ -20,25 +19,20 @@ StateMachine::StateMachine()
   deserializeTransitions("/device_settings.json");
 }
 
-// Destructor for StateMachine class
 StateMachine::~StateMachine() {
   // Cleanup if necessary
 }
 
-// Deserialize transitions from JSON file
 void StateMachine::deserializeTransitions(const char *filename) {
-  // Open the file
   File file = LittleFS.open(filename, "r");
   if (!file) {
     Serial.println("Failed to open file for reading");
     return;
   }
 
-  // Estimate the size of the JSON document
   size_t fileSize = file.size();
-  JsonDocument doc;
+  DynamicJsonDocument doc(fileSize);
 
-  // Parse the JSON
   DeserializationError error = deserializeJson(doc, file);
   if (error) {
     Serial.print("Failed to read file, using default configuration: ");
@@ -48,110 +42,96 @@ void StateMachine::deserializeTransitions(const char *filename) {
 
   JsonArray transitions = doc["transitions"].as<JsonArray>();
 
-  size_t tr_id = 0;
+  transition_table.clear();
   for (JsonObject transition : transitions) {
-    if (tr_id >= transition_table.size())
-      break;
-
     State current_state =
         stringToState(transition["current_state"].as<const char *>());
     Event event = stringToEvent(transition["event"].as<const char *>());
     State next_state =
         stringToState(transition["next_state"].as<const char *>());
-    Event next_event =
-        stringToEvent(transition["next_event"].as<const char *>());
 
-    // Placeholder actions and guards
     ActionFunction action = []() {};
     GuardFunction guard = []() { return true; };
 
-    // Check if action or guard are provided
     if (!transition["action"].isNull()) {
-      // Replace with actual action implementation
       action = []() { Serial.println("Action executed"); };
     }
 
     if (!transition["guard"].isNull()) {
-      // Replace with actual guard implementation
       guard = []() -> bool {
         Serial.println("Guard checked");
         return true;
       };
     }
 
-    StateMachine::transition_table[tr_id++] = {current_state, event, next_state,
-                                               action, guard};
+    transition_table.push_back(
+        {current_state, event, next_state, action, guard});
   }
 }
 
 void StateMachine::handleEvent(Event event) {
-  std::cout << "Handling event: " << eventToString(event)
-            << " from state: " << stateToString(current_state) << std::endl;
+  Serial.printf("Handling event: %s from state: %s\n", eventToString(event),
+                stateToString(current_state));
 
   if (event == Event::SYSTEM_FAULT) {
-    setState(State::hibernate);
-    std::cout << "Transitioned to state: " << stateToString(current_state)
-              << std::endl;
+    handleSystemFaultEvent();
     return;
   }
 
   if (event == Event::BROKER_DOWN) {
     setState(State::disconnected);
-    std::cout << "Transitioned to state: " << stateToString(current_state)
-              << std::endl;
     return;
   }
 
-  // Special handling for the RETRY event to ensure retry logic is applied
   if (event == Event::RETRY) {
-    std::cout << "Retry count before increment: " << retry_count.load()
-              << std::endl;
-    if (retry_count.load() >= max_retries) {
-      std::cout << "Max retries reached! " << retry_count.load() + 1
-                << std::endl;
-      setState(State::timeout);
-      std::cout << "Transitioned to state: " << stateToString(current_state)
-                << std::endl;
-      return;
-    } else {
-      retry_count++;
-      std::cout << "Retry count after increment: " << retry_count.load()
-                << std::endl;
-      setState(State::reconnect);
-    }
+    handleRetryEvent();
+    return;
   }
 
   for (const auto &transition : transition_table) {
     if (transition.current_state == current_state &&
         transition.event == event) {
       if (transition.guard()) {
-        if (event == Event::DISCONNECTED) {
-          if (current_state.load() != State::reconnect) {
-            retry_count = 0;
-          }
-
-          if (current_state.load() == State::mqtt_ok) {
-            setState(State::reconnect);
-          }
+        if (event == Event::DISCONNECTED && current_state != State::reconnect) {
+          retry_count = 0;
         }
 
         setState(transition.next_state);
         transition.action();
-        std::cout << "Transitioned to state: " << stateToString(current_state)
-                  << std::endl;
         return;
       }
     }
   }
 }
 
+void StateMachine::handleRetryEvent() {
+  Serial.printf("Retry count before increment: %d\n", retry_count.load());
+  if (retry_count.load() >= max_retries) {
+    Serial.printf("Max retries reached! %d\n", retry_count.load() + 1);
+    setState(State::timeout);
+  } else {
+    retry_count++;
+    Serial.printf("Retry count after increment: %d\n", retry_count.load());
+    setState(State::reconnect);
+  }
+}
+
+void StateMachine::handleSystemFaultEvent() { setState(State::hibernate); }
+
 StateMachine::State StateMachine::getCurrentState() const {
   return current_state.load();
 }
 
 void StateMachine::setState(State new_state) {
+  State old_state = current_state.load();
   current_state.store(new_state);
+  logStateTransition(old_state, new_state, Event::NONE);
   saveState(new_state);
+}
+
+void StateMachine::logStateTransition(State from, State to, Event event) {
+  Serial.printf("Transitioned from state: %s to state: %s on event: %s\n",
+                stateToString(from), stateToString(to), eventToString(event));
 }
 
 const char *StateMachine::stateToString(State state) {
@@ -186,35 +166,37 @@ const char *StateMachine::stateToString(State state) {
     return "unknown";
   }
 }
-StateMachine::State stringToState(const char *str) {
+
+StateMachine::State StateMachine::stringToState(const char *str) {
   if (strcmp(str, "disconnected") == 0)
-    return StateMachine::State::disconnected;
+    return State::disconnected;
   if (strcmp(str, "connectingTcp1") == 0)
-    return StateMachine::State::connectingTcp1;
+    return State::connectingTcp1;
   if (strcmp(str, "connectingTcp2") == 0)
-    return StateMachine::State::connectingTcp2;
+    return State::connectingTcp2;
   if (strcmp(str, "connectingMqtt") == 0)
-    return StateMachine::State::connectingMqtt;
+    return State::connectingMqtt;
   if (strcmp(str, "connected") == 0)
-    return StateMachine::State::connected;
+    return State::connected;
   if (strcmp(str, "mqtt_ok") == 0)
-    return StateMachine::State::mqtt_ok;
+    return State::mqtt_ok;
   if (strcmp(str, "disconnectingMqtt1") == 0)
-    return StateMachine::State::disconnectingMqtt1;
+    return State::disconnectingMqtt1;
   if (strcmp(str, "disconnectingMqtt2") == 0)
-    return StateMachine::State::disconnectingMqtt2;
+    return State::disconnectingMqtt2;
   if (strcmp(str, "disconnectingTcp1") == 0)
-    return StateMachine::State::disconnectingTcp1;
+    return State::disconnectingTcp1;
   if (strcmp(str, "disconnectingTcp2") == 0)
-    return StateMachine::State::disconnectingTcp2;
+    return State::disconnectingTcp2;
   if (strcmp(str, "reconnect") == 0)
-    return StateMachine::State::reconnect;
+    return State::reconnect;
   if (strcmp(str, "timeout") == 0)
-    return StateMachine::State::timeout;
+    return State::timeout;
   if (strcmp(str, "hibernate") == 0)
-    return StateMachine::State::hibernate;
-  return StateMachine::State::disconnected; // Default to disconnected state
+    return State::hibernate;
+  return State::disconnected;
 }
+
 const char *StateMachine::eventToString(Event event) {
   switch (event) {
   case Event::NONE:
@@ -239,22 +221,22 @@ const char *StateMachine::eventToString(Event event) {
     return "DELETED";
   case Event::RETRY:
     return "RETRY";
-  case Event::RETRY_OK:
-    return "RETRY OK";
-  case Event::RETRY_TCP1_OK:
-    return "RETRY TCP1 OK";
-  case Event::RETRY_TCP2_OK:
-    return "RETRY TCP2 OK";
   case Event::RETRY_MQTT_OK:
-    return "RETRY MQTT OK";
+    return "RETRY_MQTT_OK";
+  case Event::RETRY_TCP1_OK:
+    return "RETRY_TCP1_OK";
+  case Event::RETRY_TCP2_OK:
+    return "RETRY_TCP2_OK";
+  case Event::RETRY_OK:
+    return "RETRY_OK";
   case Event::MAX_RETRIES:
     return "MAX_RETRIES";
   case Event::SYSTEM_FAULT:
     return "SYSTEM_FAULT";
   case Event::BROKER_DOWN:
-    return "BROKER DOWN";
+    return "BROKER_DOWN";
   case Event::BAD_PROTOCOL:
-    return "BAD PROTOCOL";
+    return "BAD_PROTOCOL";
   case Event::RESTART:
     return "RESTART";
   case Event::RESET:
@@ -263,6 +245,7 @@ const char *StateMachine::eventToString(Event event) {
     return "unknown";
   }
 }
+
 StateMachine::Event StateMachine::stringToEvent(const char *str) {
   if (strcmp(str, "NONE") == 0)
     return Event::NONE;
@@ -306,7 +289,70 @@ StateMachine::Event StateMachine::stringToEvent(const char *str) {
     return Event::RESTART;
   if (strcmp(str, "RESET") == 0)
     return Event::RESET;
-  return Event::NONE; // Default to NONE if not matched
+  return Event::NONE;
+}
+
+void StateMachine::serializeTransitions(const char *filename) {
+  File file = LittleFS.open(filename, "w");
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  DynamicJsonDocument doc(1024);
+
+  JsonArray transitions = doc.createNestedArray("transitions");
+
+  for (const auto &transition : transition_table) {
+    JsonObject transitionObj = transitions.createNestedObject();
+    transitionObj["current_state"] = stateToString(transition.current_state);
+    transitionObj["event"] = eventToString(transition.event);
+    transitionObj["next_state"] = stateToString(transition.next_state);
+  }
+
+  if (serializeJson(doc, file) == 0) {
+    Serial.println("Failed to write to file");
+  }
+
+  file.close();
+}
+
+void StateMachine::saveState(State state) {
+  File file = LittleFS.open("/current_state.json", "w");
+  if (!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+
+  DynamicJsonDocument doc(1024);
+  doc["current_state"] = stateToString(state);
+
+  if (serializeJson(doc, file) == 0) {
+    Serial.println("Failed to write to file");
+  }
+
+  file.close();
+}
+
+StateMachine::State StateMachine::loadState() {
+  File file = LittleFS.open("/current_state.json", "r");
+  if (!file) {
+    Serial.println("Failed to open file for reading");
+    return State::disconnected;
+  }
+
+  size_t fileSize = file.size();
+  DynamicJsonDocument doc(fileSize);
+
+  DeserializationError error = deserializeJson(doc, file);
+  if (error) {
+    Serial.print("Failed to read file, using default configuration: ");
+    Serial.println(error.c_str());
+    return State::disconnected;
+  }
+
+  const char *stateStr = doc["current_state"].as<const char *>();
+  return stringToState(stateStr);
 }
 
 } // namespace MQTTCore
--- src/NestMQTT/MQTTCore/MQTTStateMachine.h
+++ src/NestMQTT/MQTTCore/MQTTStateMachine.h
@@ -5,7 +5,10 @@
 #include <atomic>
 #include <cstddef>
 #include <functional>
+#include <iostream>
 #include <utility>
+#include <vector>
+
 
 namespace MQTTCore {
 
@@ -13,7 +16,6 @@ class StateMachine {
 
 public:
   enum class State {
-
     disconnected = 0,
     connectingTcp1 = 1,
     connectingTcp2 = 2,
@@ -27,7 +29,6 @@ public:
     reconnect = 10,
     timeout = 11,
     hibernate = 12,
-
   };
 
   enum class Event {
@@ -56,25 +57,13 @@ public:
 
   using GuardFunction = std::function<bool()>;
   using ActionFunction = std::function<void()>;
+
   struct Transition {
     State current_state;
     Event event;
     State next_state;
     ActionFunction action;
     GuardFunction guard;
-  };
-
-  template <typename... Args>
-  static std::array<Transition, sizeof...(Args)> TransitionTable() {
-    return {Args::get_transition()...};
-  }
-
-  template <State Start, Event EventTrigger, State Next, Event ActionEvent>
-  struct Row {
-    static Transition get_transition() {
-      return {Start, EventTrigger, Next, []() { /* Action */ },
-              []() { return true; }};
-    }
   };
 
   StateMachine();
@@ -84,19 +73,15 @@ public:
   void handleEvent(Event event);
 
   State getCurrentState() const;
+
   void serializeTransitions(const char *filename);
   void deserializeTransitions(const char *filename);
 
-  void saveState(State state) {
-    // Save to persistent storage
-  }
-
-  State loadState() {
-    // Load from persistent storage
-    return State::disconnected;
-  }
+  void saveState(State state);
+  State loadState();
 
   void setState(State new_state);
+
 #ifdef UNIT_TEST
   void setMockAction(ActionFunction mock_action) {
     this->mock_action = mock_action;
@@ -109,86 +94,11 @@ private:
   std::atomic<State> current_state;
   std::atomic<int> retry_count;
   const int max_retries = 3;
-  std::array<Transition, 40> transition_table;
-  // const std::array<Transition, 40> transition_table = TransitionTable<
-  //     // hibernate  transitions
-  //     Row<State::hibernate, Event::RESTART, State::disconnected,
-  //     Event::NONE>, Row<State::disconnected, Event::SYSTEM_FAULT,
-  //     State::hibernate,
-  //         Event::NONE>,
-  //     Row<State::timeout, Event::SYSTEM_FAULT, State::hibernate,
-  //     Event::NONE>,
+  std::vector<Transition> transition_table;
 
-  //     // Initial transitions
-  //     Row<State::disconnected, Event::ERROR, State::disconnected,
-  //     Event::NONE>, Row<State::timeout, Event::ERROR, State::disconnected,
-  //     Event::NONE>, Row<State::timeout, Event::RESET, State::disconnected,
-  //     Event::NONE>, Row<State::disconnected, Event::BEFORE_CONNECT,
-  //     State::connectingTcp1,
-  //         Event::NONE>,
-  //     // Connection phase transitions
-  //     Row<State::connectingTcp1, Event::CONNECTED, State::connectingTcp2,
-  //         Event::NONE>,
-  //     Row<State::connectingTcp2, Event::CONNECTED, State::connectingMqtt,
-  //         Event::NONE>,
-  //     Row<State::connectingMqtt, Event::CONNECTED, State::connected,
-  //         Event::NONE>,
-  //     // MQTT SUCCESS
-  //     Row<State::connected, Event::SUBSCRIBED, State::mqtt_ok, Event::NONE>,
-  //     Row<State::connected, Event::UNSUBSCRIBED, State::mqtt_ok,
-  //     Event::NONE>, Row<State::connected, Event::PUBLISHED, State::mqtt_ok,
-  //     Event::NONE>, Row<State::connected, Event::DATA, State::mqtt_ok,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::SUBSCRIBED, State::mqtt_ok,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::UNSUBSCRIBED, State::mqtt_ok,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::PUBLISHED, State::mqtt_ok,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::DATA, State::mqtt_ok,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::DELETED, State::mqtt_ok,
-  //     Event::NONE>,
-  //     // BAD PROTOCOL
-  //     Row<State::mqtt_ok, Event::BAD_PROTOCOL, State::connected,
-  //     Event::NONE>, Row<State::mqtt_ok, Event::ERROR, State::connected,
-  //     Event::NONE>,
-  //     // Disconnection phase transitions
-  //     Row<State::connected, Event::DISCONNECTED, State::disconnectingMqtt1,
-  //         Event::NONE>,
-  //     Row<State::disconnectingMqtt1, Event::DISCONNECTED,
-  //         State::disconnectingMqtt2, Event::NONE>,
-  //     Row<State::disconnectingMqtt2, Event::DISCONNECTED,
-  //         State::disconnectingTcp1, Event::NONE>,
-  //     Row<State::disconnectingTcp1, Event::DISCONNECTED,
-  //         State::disconnectingTcp2, Event::NONE>,
-  //     Row<State::disconnectingTcp2, Event::DISCONNECTED, State::disconnected,
-  //         Event::NONE>,
-  //     // Reconnect phase entry
-  //     Row<State::mqtt_ok, Event::DISCONNECTED, State::reconnect,
-  //     Event::NONE>, Row<State::disconnected, Event::RETRY, State::reconnect,
-  //     Event::NONE>, Row<State::disconnectingTcp1, Event::RETRY,
-  //     State::reconnect,
-  //         Event::NONE>,
-  //     Row<State::disconnectingTcp2, Event::RETRY, State::reconnect,
-  //         Event::NONE>,
-  //     Row<State::disconnectingMqtt1, Event::RETRY, State::reconnect,
-  //         Event::NONE>,
-  //     Row<State::disconnectingMqtt2, Event::RETRY, State::reconnect,
-  //         Event::NONE>,
-  //     Row<State::timeout, Event::RETRY, State::timeout, Event::NONE>,
-  //     // Retry Success
-
-  //     Row<State::reconnect, Event::RETRY_OK, State::connectingTcp1,
-  //         Event::NONE>,
-  //     Row<State::reconnect, Event::RETRY_TCP1_OK, State::connectingTcp2,
-  //         Event::NONE>,
-  //     Row<State::reconnect, Event::RETRY_TCP2_OK, State::connectingMqtt,
-  //         Event::NONE>,
-  //     Row<State::reconnect, Event::RETRY_MQTT_OK, State::connected,
-  //         Event::NONE>,
-  //     // Retry UNSUCCESS
-  //     Row<State::reconnect, Event::RETRY, State::reconnect, Event::NONE>,
-  //     Row<State::reconnect, Event::MAX_RETRIES, State::timeout, Event::NONE>,
-
-  //     // default state Disconnected
-  //     Row<State::disconnected, Event::DISCONNECTED, State::disconnected,
-  //         Event::NONE>>();
+  void handleRetryEvent();
+  void handleSystemFaultEvent();
+  void logStateTransition(State from, State to, Event event);
 
   static const char *stateToString(State state);
   static const char *eventToString(Event event);
